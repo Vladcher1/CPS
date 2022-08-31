7099b9ddbfe6cccd13ae824bb18b5128133f675c
Vlada Cherepanova <vlada.hero@inbox.ru>
2022-08-31 18:28:01 +0500

    ustala

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,23 @@
-console.log("ssssssss");
+const swiper = new Swiper('.swiper', {
+   // Optional parameters
+   direction: 'horizontal',
+   loop: true,
+
+ 
+   // If we need pagination
+   pagination: {
+     el: '.swiper-pagination',
+   },
+ 
+   // Navigation arrows
+   navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    slidesPerView: 1.2,
+    
+   // And if we need scrollbar
+   scrollbar: {
+     el: '.swiper-scrollbar',
+   },
+ });
