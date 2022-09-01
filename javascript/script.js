3febae356dec2ee19856a98cca4e539dab528ba2
Vlada Cherepanova <vlada.hero@inbox.ru>
2022-09-01 13:44:08 +0500

    sg,d

--- javascript/script.js
+++ javascript/script.js
@@ -1,12 +1,15 @@
-const swiper = new Swiper('.swiper', {
-   // Optional parameters
+let swiper = new Swiper('.swiper', {
+    // Optional parameters
    direction: 'horizontal',
    loop: true,
+   slidesPerView: 'auto' ,
+   spaceBetween: 16,
 
  
    // If we need pagination
    pagination: {
      el: '.swiper-pagination',
+     clickable: true,
    },
  
    // Navigation arrows
@@ -14,10 +17,33 @@ const swiper = new Swiper('.swiper', {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
-    slidesPerView: 1.2,
+    
     
    // And if we need scrollbar
    scrollbar: {
      el: '.swiper-scrollbar',
    },
- });
+  });
+
+
+
+
+
+// let brandsList = document.querySelector('.brands__list'); //swiper
+// let wrapper = document.querySelector('div');
+// let buttons = document.querySelectorAll('li');
+//  const windowSize = window.matchMedia("(max-width: 768px)");
+
+//  console.log(wrapper);
+
+
+window.addEventListener('resize', function () {
+  let brandsList = document.querySelector('.brands__list'); 
+const mediaQuery = window.matchMedia('(max-width: 768px)');
+
+
+
+swiper.destroy(true, true);
+brandsList.classList.remove('swiper');
+  });
+
