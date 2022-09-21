53f0c719715319e8ae63c233328176d12aff9d03
Vlada Cherepanova <vlada.hero@inbox.ru>
2022-09-21 14:34:36 +0500

    ready modals on button

--- block7/src/js/modalsOn.js
+++ block7/src/js/modalsOn.js
@@ -3,8 +3,9 @@ const modalReport = document.querySelector('.modal-report');
 const callButton = document.querySelector('.order-button');
 const modalOrder = document.querySelector('.modal-order');
 const modal = document.querySelector('.modal');
+const modal1 = document.querySelector('.modal-1');
 const closeButton = document.querySelector('.close-button');
-
+console.log(modal1);
 const container = document.querySelector('.flexbox-container');
 
 // for (let modal of modals){
@@ -15,12 +16,14 @@ chatButton.addEventListener('click', function () {
 
 
 callButton.addEventListener('click', function () {
-   modal.classList.toggle('hidden');
+   modal1.classList.toggle('hidden');
    
   container.classList.toggle('container-visually');
  });
 // }
 
+
+const chooseButton 
  closeButton.addEventListener('click', function () {
   modal.classList.toggle('hidden');
   container.classList.toggle('container-visually');
