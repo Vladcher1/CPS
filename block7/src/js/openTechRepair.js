791d0dba1153dec171ad64e8635873a6992a6f2f
Vlada Cherepanova <vlada.hero@inbox.ru>
2022-09-22 11:35:49 +0500

    techrepair oprn all script

--- block7/src/js/openTechRepair.js
+++ block7/src/js/openTechRepair.js
@@ -1,27 +1,43 @@
-const techRepair = document.querySelector('.tech-repair');
-const techContainer = document.querySelector('.tech-container');
-const listElements = techContainer.children;
+const techRepair = document.querySelector('.tech-repair')
+const techContainer = document.querySelector('.tech-container')
+const list = techRepair.querySelector('.brands__list')
+const listElements = techContainer.children
 const media768 = window.matchMedia('(min-width: 768px)')
 const media1120 = window.matchMedia('(min-width: 1120px)')
-const buttonClose = techRepair.querySelector('.second-title');
-const icon = techRepair. querySelector('.second-title-icon');
-console.log(icon);
+const buttonClose = techRepair.querySelector('.second-title')
+const icon = techRepair.querySelector('.second-title-icon')
+const buttonCloseText = buttonClose.querySelector('p')
+console.log(list)
+
+// по умолчанию все элементы после 3 скрыты
 if (media768.matches && !media1120.matches) {
-for (let i = 3; i <= listElements.length - 1; i++ ){
-
-   listElements[i].classList.add('hidden');
+  for (let i = 3; i <= listElements.length - 1; i++) {
+    listElements[i].classList.add('hidden')
+  }
 }
-} 
 
+// при нажатии на кнопку раскрыть все открываются все
+// элементы
 buttonClose.addEventListener('click', function () {
-   if (icon.classList.contains('button-not-rotate')) {
-     icon.classList.add('button-rotate')
-     icon.classList.remove('button-not-rotate')
-   //   buttonClose.textContent = 'Скрыть';
-   } else {
-     icon.classList.add('button-not-rotate')
-     icon.classList.remove('button-rotate')
-   //   buttonClose.textContent = 'Показать всё';
-   }
- })
+  if (list.classList.contains('button-close')) {
+    list.classList.remove('button-close')
+    list.classList.add('button-open')
+    icon.classList.add('button-rotate')
+    icon.classList.remove('button-not-rotate')
+    buttonCloseText.textContent = 'Скрыть'
 
+    for (let i = 0; i <= listElements.length; i++) {
+      listElements[i].classList.remove('hidden')
+    }
+  } else if (list.classList.contains('button-open')) {
+    list.classList.add('button-close')
+    list.classList.remove('button-open')
+    icon.classList.add('button-not-rotate')
+    icon.classList.remove('button-rotate')
+    buttonCloseText.textContent = 'Показать всё'
+
+    for (let i = 3; i <= listElements.length - 1; i++) {
+      listElements[i].classList.add('hidden')
+    }
+  }
+})
