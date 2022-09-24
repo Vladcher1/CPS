158cf3f47439dd21dd0cd941903945b04dcea1a9
Vlada Cherepanova <vlada.hero@inbox.ru>
2022-09-24 13:18:36 +0500

    a

--- block7/src/js/menuOn.js
+++ block7/src/js/menuOn.js
@@ -1,30 +1,18 @@
-const menuButton = document.querySelector('.page-header__icon-menu');
-const modal = document.querySelector('.modal-3');
-const closeButton = document.querySelector('.modal-header__opened-menu');
-console.log(closeButton);
+const menuButton = document.querySelector('.page-header__icon-menu')
+const modal = document.querySelector('.modal-menu')
+const closeButton = document.querySelector('.modal-header__opened-menu')
+console.log(closeButton)
 
 menuButton.addEventListener('click', function () {
-
-   modal.classList.remove('hidden');
+  modal.classList.remove('hidden')
 })
 
-// menuButton.addEventListener('click', function () {
-
-//    modal.style.display = "block";
-// })
-
-// closeButton.addEventListener('click', function () {
-
-//    modal.style.display = "none";
-// })
-
 closeButton.addEventListener('click', function () {
+  modal.classList.add('hidden')
+})
 
-      modal.classList.add('hidden');
-   })
+const media1440 = window.matchMedia('(min-width: 1440px)')
 
-   const media1440 = window.matchMedia('(min-width: 1440px)');
-
-   if (media1440.matches){
-      modal.classList.remove('hidden');
-   }
+if (media1440.matches) {
+  modal.classList.remove('hidden')
+}
